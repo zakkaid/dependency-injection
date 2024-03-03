18ddfac86173253375dd5eb20a75ccfcdeb562dd
zakkaid <zakaria.bakaid123@gmail.com>
2024-03-03 19:11:46 +0100

    refactor

--- DI/src/Dynamique/Test.java
+++ DI/src/Dynamique/Test.java
@@ -10,22 +10,12 @@ public class Test {
     public static void main(String[] args) {
         try{
             Scanner sc = new Scanner(new File("src/config.txt"));
-
-            String className = sc.nextLine();
-            Class clsab = Class.forName(className);
-            Ability ability =  (Ability) clsab.newInstance();
-
-             className = sc.nextLine();
-             clsab = Class.forName(className);
-             Player player =  (Player) clsab.newInstance();
-
-             player.setAbility(ability);
-             player.perform();
-
-
+            Ability ability = (Ability) Class.forName(sc.nextLine()).newInstance();
+            Player player = (Player) Class.forName(sc.nextLine()).newInstance();
+            player.setAbility(ability);
+            player.perform();
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-
     }
 }
